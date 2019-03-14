65d3459fd87a3a454f88332120fca5ca1e4f0b22
Jeff M <jkdmarks@gmail.com>
2019-03-14 17:28:04 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,14 +1,17 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+def songs
+  array = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
+  array
+end
 
 def help
   puts "I accept the following commands
@@ -20,5 +23,14 @@ end
 
 
 def play
-  puts "hi"
+  puts "Please enter the number or full name of a track."
+  song = gets
+  
+  if song.length == 1
+    
+  elsif songs.include(song)
+  
+  else
+    
+  end
 end
